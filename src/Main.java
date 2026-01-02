42d7df68cf6e8f2bf3ea814235536f04dfc14697
MaxSmolenskiy <ivanmax97@gmail.com>
2026-01-02 17:33:01 +0300

    "проверка наличия файла"

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,24 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число: ");
-        int x = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число: ");
-        int y = new Scanner(System.in).nextInt();
-        System.out.println("Сумма ваших чисел: " + (x+y));
-        System.out.println("Разность ваших чисел: " + (x-y));
-        System.out.println("Произведение ваших чисел: " + (x*y));
-        System.out.println("Частное ваших чисел: " + ((double)(x)/y));
+        int i = 1;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!fileExists || isDirectory) {
+                System.out.println("Указанный файл не существует или указанный путь является путём к папке");
+                continue;
+
+            }
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + i);
+            i++;
+        }
+
+
     }
 }
